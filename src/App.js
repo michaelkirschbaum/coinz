6e1c1869014f45870d1078a9e440d3cc10e107c7
Michael Kirschbaum <hi@michaelkirschbaum.com>
2019-07-03 18:26:52 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import {hot} from "react-hot-loader";
+import React from 'react'
+import {
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+import { hot } from 'react-hot-loader'
+import Login from './Login';
+import Account from './Account'
+
+const ProtectedRoute = ({ path, component: Component, auth }) => (
+  <Route
+    {...path}
+    render={props => auth ?
+      <Component /> :
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: { from: props.location }
+        }}
+      />
+    }
+  />
+)
 
 const App = () => (
-  <div></div>
-);
+  <div>
+    <Switch>
+      <Route path='/login' component={Login} />
+      <ProtectedRoute exact path='/' component={Account} auth={true} />
+    </Switch>
+  </div>
+)
 
-export default hot(module)(App);
+export default hot(module)(App)
