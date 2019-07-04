e6ad6c572d0fcc489f48158b4469a3733afcdbaa
Michael Kirschbaum <hi@michaelkirschbaum.com>
2019-07-03 20:08:37 -0700

    add Header.js

---
+++ src/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <React.Fragment>
+    <div>User</div>
+    <div>Sign Out</div>
+  </React.Fragment>
+);
+ export default Header
---
+++ src/Header/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Header from './Header'
+
+describe('Header', () => {
+  it('should match the snapshot', () => {
+    const header = shallow(
+      <Header />,
+    )
+
+    expect(header).toMatchSnapshot()
+  })
+})
