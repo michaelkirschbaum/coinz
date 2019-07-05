8010388c47d3351ca8af75a5a44f41dab8eb4d53
Michael Kirschbaum <hi@michaelkirschbaum.com>
2019-07-05 12:21:42 -0700

    add state variables

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,18 @@
 /** @jsx jsx */
-import React from 'react'
+import React, { useState } from 'react'
 import { css, jsx } from '@emotion/core'
 
-const Login = () => (
-  <>
-    <div>Sign In</div>
-    <div>Jobcoin Address</div>
-    <input />
-    <button />
-  </>
-)
+const Login = () => {
+  const address = useState(null)
+
+  return (
+    <>
+      <div>Sign In</div>
+      <div>Jobcoin Address</div>
+      <input />
+      <button />
+    </>
+  )
+}
 
 export default Login
--- src/components/Send.js
+++ src/components/Send.js
@@ -1,24 +1,29 @@
 /** @jsx jsx */
-import React from 'react'
+import React, { useState } from 'react'
 import { css, jsx } from '@emotion/core'
 
-class Send = () => (
-  <div
-    css={css`
-      display: grid;
-    `}
-  >
-    <div>Send Jobcoin</div>
-    <div>
-      Destination Address
-      <input />
+const Send = () => {
+  const sendAddress = useState(null)
+  const amount = useState(null)
+
+  return (
+    <div
+      css={css`
+        display: grid;
+      `}
+    >
+      <div>Send Jobcoin</div>
+      <div>
+        Destination Address
+        <input />
+      </div>
+      <div>
+        Amount
+        <input />
+      </div>
+      <button>Send</button>
     </div>
-    <div>
-      Amount
-      <input />
-    </div>
-    <button>Send</button>
-  </div>
-)
+  )
+}
 
 export default Send
