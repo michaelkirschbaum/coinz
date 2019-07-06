0d09a553f5b2acf97a1955ce3f555d877ba5c82f
Michael Kirschbaum <hi@michaelkirschbaum.com>
2019-07-05 17:08:24 -0700

    add d3

--- src/components/History.js
+++ src/components/History.js
@@ -1,9 +1,15 @@
 /** @jsx jsx */
 import React from 'react'
 import { css, jsx } from '@emotion/core'
+import * as d3 from 'd3'
+
+const API_URL = 'https://jobcoin.gemini.com/customary/api' // TODO: remove
+const route = '/transactions'
 
 const History = () => (
-  <div css={css``}>History</div>
+  <div css={css``}>
+    History
+  </div>
 )
 
 export default History
