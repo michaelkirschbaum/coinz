8274c02d19ee656caacbce6874d539fd9b46dd05
Michael Kirschbaum <hi@michaelkirschbaum.com>
2019-07-15 11:09:59 -0700

    add preset

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"]
+            presets: ["@babel/preset-env", "es2015"]
           }
         }
       },
